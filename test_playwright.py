3b6f15215f669873106b727f3d09266228868d53
Jiacheng Cao <152639220+Omegcjc@users.noreply.github.com>
2025-04-10 12:49:27 +0800

    Update test_playwright.py

--- test_playwright.py
+++ test_playwright.py
@@ -2,7 +2,7 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False， channel = "chrome") # 使用本地Chrome
+    browser = p.chromium.launch(headless=False, channel = "chrome") # 使用本地Chrome
     page = browser.new_page()
     page.goto("https://www.baidu.com")
     print(page.title())
